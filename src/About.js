905cdc1b5287086461cbd1ca27ada23fba407c85
thanoskat <thanoskat@gmx.com>
2021-11-16 21:37:03 +0200

    test api in about.js

--- src/About.js
+++ src/About.js
@@ -1,9 +1,27 @@
 import './App.css';
+import axios from 'axios'
+import { useState, useEffect } from 'react'
 
 function About() {
+  const [text, setText] = useState('')
+  useEffect(() => {
+    lala()
+  }, [])
+
+  const lala = async () => {
+    try{
+      const res = await axios.get('http://localhost:4000')
+      setText(res.data)
+    }
+    catch(error){
+      console.dir("ABOUT: ", error)
+      setText(error.message)
+    }
+  }
+
   return (
     <div>
-      <h1>About Page</h1>
+      <h1>{text}</h1>
     </div>
   );
 }
