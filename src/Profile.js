c33c10dd0806dcb31fe491f1541b861ceb8dd234
ChristosKehayas <christos.kechagia@gmail.com>
2021-11-17 21:43:14 +0000

    creates group

--- src/Profile.js
+++ src/Profile.js
@@ -2,12 +2,17 @@ import './App.css';
 import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import useAxios from './utility/useAxios'
+import {Form, Button} from 'semantic-ui-react'
+
+
 
 function Profile(){
 
   const [user, setUser] = useState({})
+  const [groupName, setGroupName] = useState("")
 
   const api = useAxios()
+  const {Group, Input}=Form;
 
   useEffect(() => {
     fetchUser()
@@ -24,13 +29,34 @@ function Profile(){
     }
   }
 
+  const  onSubmitFunction = async (e)=>{
+
+    const groupObj={
+      title:groupName
+    }
+
+    await api.post('/groups/creategroup', groupObj) 
+    e.target.reset()
+
+  }
 
     return(
-        <div>
-      {user._id}
-             
-        </div>
+
+    <Form 
+      onSubmit={e=>onSubmitFunction(e)}
+      style={{paddingLeft: '100px', paddingRight: '100px',paddingTop:'10px'}}>
+   <Group widths='equal'>
+  <Input
+      onChange={(event)=>setGroupName(event.target.value)}
+      fluid
+      placeholder='Name of Group'
+    />
+  </Group>
+  <Button primary>Create Group</Button>
+  
+</Form>
     )
+
 
 }
 
