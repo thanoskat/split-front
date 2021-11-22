6ebea6a516879a8b70998cc8adb81c715c529fff
thanoskat <thanoskat@gmx.com>
2021-11-22 18:39:02 +0200

    profile render syntax

--- src/Profile.js
+++ src/Profile.js
@@ -39,7 +39,7 @@ function Profile(){
 
   const fetchUser = async () => {
     try{
-      const response = await api.get('/getusers/profile')  
+      const response = await api.get('/getusers/profile')
       console.log(response.data.groups)
       console.log(response.data)
       setGroupInfo(response.data.groups)
@@ -54,7 +54,7 @@ function Profile(){
     const groupObj={
       title:groupName
     }
-    await api.post('/groups/creategroup', groupObj) 
+    await api.post('/groups/creategroup', groupObj)
     e.target.reset()
   }
 
@@ -68,100 +68,48 @@ const onSubmitAddUserToGroup = async (e)=>{
   await api.post('groups/addUserToGroup', IDs)
   e.target.reset()
 }
-  
 
-
-    return(
-      <div>
-    <Form 
+  return(
+  <div>
+    <Form
       onSubmit={e=>onSubmitFunction(e)}
       style={{paddingLeft: '100px', paddingRight: '100px',paddingTop:'10px'}}>
-
-        <CustomCard 
-              nickname={userInfo.nickname} 
-              email = {userInfo.email} 
-              _id={userInfo._id} 
-              length={groupInfo.length}
-              groupInfo={groupInfo}
-             />
-
-   <Group widths='equal'>
-   
-  <Input
-      onChange={(event)=>setGroupName(event.target.value)}
-      fluid
-      placeholder='Name of Group'
-    />
-
- 
-
-
-  </Group>
-  <Button primary>Create Group</Button>
- 
-  
-</Form>
-
-
-
-<Form 
+      <CustomCard
+        nickname={userInfo.nickname}
+        email = {userInfo.email}
+        _id={userInfo._id}
+        length={groupInfo.length}
+        groupInfo={groupInfo}/>
+      <Group widths='equal'>
+        <Input
+          onChange={(event)=>setGroupName(event.target.value)}
+          fluid
+          placeholder='Name of Group'/>
+      </Group>
+      <Button primary>Create Group</Button>
+    </Form>
+    <Form
       onSubmit={e=>onSubmitAddUserToGroup(e)}
       style={{paddingLeft: '100px', paddingRight: '100px',paddingTop:'10px'}}>
-
-   <Group widths='equal'>
-   
-   <Input
-      onChange={event=>SetUserIDtoBeAdded(event.target.value)}
-      fluid
-      placeholder='Id of User to be added'
-    />
-  <Input
-      onChange={event=>SetGroupIDtoAddUser(event.target.value)}
-      fluid
-      placeholder='Group user will be added to '
-    />  
-  </Group>
-  <Button primary>Add User to Group</Button> 
-</Form>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-          <h1>
-           {groupInfo.map(group=>(
-            <h2>group ID : {group._id} </h2>
-            
-          ))}
-
-          {groupInfo.map(group=>(
-            <h2>group title : {group.title} </h2>
-            
-          ))}
-    
-         {users.map(user=>(
-                <h2>users : {user._id} </h2>
-            
-           ))}  
-          
-          </h1>
-      </div>
-    )
-
-
+      <Group widths='equal'>
+        <Input
+          onChange={event=>SetUserIDtoBeAdded(event.target.value)}
+          fluid
+          placeholder='Id of User to be added'/>
+        <Input
+            onChange={event=>SetGroupIDtoAddUser(event.target.value)}
+            fluid
+            placeholder='Group user will be added to '/>
+      </Group>
+      <Button primary>Add User to Group</Button>
+      </Form>
+      <h1>
+        {groupInfo.map(group=>(<h2>group ID : {group._id} </h2>))}
+        {groupInfo.map(group=>(<h2>group title : {group.title} </h2>))}
+        {users.map(user=>(<h2>users : {user._id} </h2>))}
+      </h1>
+    </div>
+  )
 }
 
 export default Profile;
