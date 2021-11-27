b43cf1620ca3907e7eb7f3b97613ada4fd65ebff
ChristosKehayas <christos.kechagia@gmail.com>
2021-11-27 17:40:06 +0000

    fixed notifications popup

--- src/Notification.jsx
+++ src/Notification.jsx
@@ -49,7 +49,7 @@ const api = useAxios()
     return(
         <div>
             {/* <h1>{request.map(req=><h3>{req._id}</h3>)}</h1>  */}
-     <Grid columns={3} centered container>
+     {/* <Grid columns={3} centered container>
          <Grid.Row >      
                 <Popup  trigger= { <Menu compact>
                     <Menu.Item as="a" onClick={()=> setOpen(!open)}>
@@ -74,8 +74,41 @@ const api = useAxios()
       </Grid.Row>
       ):("")
       }
-    </Grid>
+    </Grid> */}
     
+    <Popup  trigger= { <Menu compact>
+                        <Menu.Item as="a" onClick={()=> setOpen(!open)}>
+                          <Icon name='bell'  />
+                          {pendingRequestsNo?(
+                          <Label color='red' floating >
+                            {pendingRequestsNo}
+                         </Label>):("")}
+                        </Menu.Item>
+                     </Menu>}
+                     on='click'
+                    >
+                   <Grid>
+                      <Grid.Column>
+                          <Popup                
+                              trigger={pendingRequestsNo? (
+                                <Grid.Row >
+                                    <Grid.Column>
+                                      {request.map((n) => 
+                                          <Segment.Group raised >
+                                            {displayNotification(n)}
+                                          </Segment.Group>)}   
+                                  </Grid.Column>     
+                                </Grid.Row>
+                                ):("")
+                                }
+                              disabled
+                              position='top center'
+                              size='tiny'
+                              inverted
+                            />
+                      </Grid.Column>
+                   </Grid>
+      </Popup>
       </div>
       
     )
