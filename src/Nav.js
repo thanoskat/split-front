3c053308be03777db87ff04a74783b5dfa3c5d5e
thanoskat <thanoskat@gmx.com>
2021-12-02 00:47:51 +0200

    Navigation menu

--- src/Nav.js
+++ src/Nav.js
@@ -10,6 +10,7 @@ function Nav() {
   const { signOut, sessionID } = useContext(AuthenticationContext)
   const [ userNickName, setUserNickName ] = useState('')
   const [ userId, setUserId ] = useState('')
+  const [activeItem, setActiveItem] = useState('Home')
 
   async function getUserInfo() {
     try {
@@ -37,79 +38,135 @@ function Nav() {
   //   getUserInfo()
   //   // eslint-disable-next-line
   // }, [])
-
+  const handleItemClick = (e, { name }) => setActiveItem(name)
   const { Column, Row } = Grid
-
   return (
   // https://react.semantic-ui.com/augmentation/
-  <Segment inverted>
-    <Grid columns='equal'>
-      <Column>
-        <Button
-          color='orange'
-          inverted
-          as={NavLink}
-          exact
-          to="/about"
-          style={isActive => ({backgroundColor:isActive ? "orange" : "purple"})}
-        >About</Button>
-      </Column>
-      <Column>
-        <Button
-          as={NavLink}
-          to='/users'
-          className={isActive => (!isActive ? 'active-button' : 'noactive-button')}
-        >Users</Button>
-      </Column>
-      <Column>
-        <Button
-          inverter
-          as={NavLink}
-          to='/login'
-          style={isActive => (
-            {
-              backgroundColor:isActive ? "orange" : "grey",
-              color:isActive ? "black" : "black",
-              borderRadius: 0,
-            }
-          )}
-        >Sign in</Button>
-      </Column>
-      <Column>
-        <Button color='twitter' as={Link} to='/signup'>Sign up</Button>
-      </Column>
-      <Column>
-        <Button onClick={onLogoutClick}>Sign out</Button>
-      </Column>
-      <Column>
-        <Button as={Link} to='/profile'>Profile</Button>
-      </Column>
-      <Column>
-        <Button as={Link} to='/mygroups'>My Groups</Button>
-      </Column>
-      <Column>
-        <Menu compact>
-          <Menu.Item as='a'>
-            <Icon name='bell'/>
-            <Label color='red' floating>
-              22
-            </Label>
-          </Menu.Item>
-        </Menu>
-      </Column>
-      <Column>
-        <Button>
-          <Button.Content>
-            <Icon name='bell'/>
-            <Label color='red' floating>
-            </Label>
-          </Button.Content>
-        </Button>
-      </Column>
-      <Column>
-        {userNickName}{'\n'}{userId}
-      </Column>
-    </Grid>
+  // <Segment inverted>
+  //   <Grid columns='equal'>
+  //     <Column>
+  //       <Button
+  //         color='orange'
+  //         inverted
+  //         as={NavLink}
+  //         exact
+  //         to="/about"
+  //         style={isActive => ({backgroundColor:isActive ? "orange" : "purple"})}
+  //       >About</Button>
+  //     </Column>
+  //     <Column>
+  //       <Button
+  //         as={NavLink}
+  //         to='/users'
+  //         className={isActive => (!isActive ? 'active-button' : 'noactive-button')}
+  //       >Users</Button>
+  //     </Column>
+  //     <Column>
+  //       <Button
+  //         inverter
+  //         as={NavLink}
+  //         to='/login'
+  //         style={isActive => (
+  //           {
+  //             backgroundColor:isActive ? "orange" : "grey",
+  //             color:isActive ? "black" : "black",
+  //             borderRadius: 0,
+  //           }
+  //         )}
+  //       >Sign in</Button>
+  //     </Column>
+  //     <Column>
+  //       <Button color='twitter' as={Link} to='/signup'>Sign up</Button>
+  //     </Column>
+  //     <Column>
+  //       <Button onClick={onLogoutClick}>Sign out</Button>
+  //     </Column>
+  //     <Column>
+  //       <Button as={Link} to='/profile'>Profile</Button>
+  //     </Column>
+  //     <Column>
+  //       <Button as={Link} to='/mygroups'>My Groups</Button>
+  //     </Column>
+  //     <Column>
+  //       <Menu icon compact>
+  //         <Menu.Item as='a'>
+  //           <Icon name='bell'/>
+  //           <Label color='red' floating>
+  //             22
+  //           </Label>
+  //         </Menu.Item>
+  //       </Menu>
+  //     </Column>
+  //     <Column>
+  //       <Button icon>
+  //         <Button.Content>
+  //           <Icon name='bell'/>
+  //           <Label color='red' floating>
+  //           </Label>
+  //         </Button.Content>
+  //       </Button>
+  //     </Column>
+  //     <Column>
+  //       {userNickName}{'\n'}{userId}
+  //     </Column>
+  //   </Grid>
+  // </Segment>
+  <Segment basic inverted>
+    <Menu inverted pointing secondary>
+      <Menu.Item
+        as={NavLink}
+        exact
+        to="/about"
+        name="about"
+        active={activeItem === 'about'}
+        onClick={handleItemClick}/>
+      <Menu.Item
+        as={NavLink}
+        exact
+        to="/users"
+        name="Users"
+        active={activeItem === 'Users'}
+        onClick={handleItemClick}/>
+      <Menu.Item
+        as={NavLink}
+        exact
+        to="/login"
+        name="signin"
+        active={activeItem === 'signin'}
+        onClick={handleItemClick}/>
+      <Menu.Item
+        as={NavLink}
+        exact
+        to="/signup"
+        name="signup"
+        active={activeItem === 'signup'}
+        onClick={handleItemClick}/>
+      <Menu.Item
+        as={NavLink}
+        exact
+        to="/mygroups"
+        name="My groups"
+        active={activeItem === 'My groups'}
+        onClick={handleItemClick}/>
+      <Menu.Item
+        as={NavLink}
+        exact
+        to="/profile"
+        name="profile"
+        active={activeItem === 'profile'}
+        onClick={handleItemClick}/>
+      <Menu.Item icon as='a'>
+        <Icon name='bell'/>
+        <Label size='mini' color='red' floating>
+          22
+        </Label>
+      </Menu.Item>
+      <Menu.Item
+        position='right'
+        name="Sign out"
+        active={activeItem === 'Sign out'}
+        onClick={onLogoutClick}/>
+    </Menu>
   </Segment>
   );
 }
