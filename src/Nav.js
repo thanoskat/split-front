4ed15c8738062d353aac93874a57410590e1b900
thanoskat <thanoskat@gmx.com>
2021-12-01 18:03:53 +0200

    Nav responsive init

--- src/Nav.js
+++ src/Nav.js
@@ -1,5 +1,6 @@
-import { Link } from 'react-router-dom'
-import { useContext } from 'react'
+import './Nav.css'
+import { Link, NavLink } from 'react-router-dom'
+import { useContext, useEffect, useState } from 'react'
 import { AuthenticationContext } from './AuthenticationContext'
 import useAxios from './utility/useAxios'
 import { Button, Grid, Segment } from 'semantic-ui-react'
@@ -7,42 +8,88 @@ import { Button, Grid, Segment } from 'semantic-ui-react'
 function Nav() {
   const api = useAxios()
   const { signOut, sessionID } = useContext(AuthenticationContext)
+  const [ userNickName, setUserNickName ] = useState('')
+  const [ userId, setUserId ] = useState('')
+
+  async function getUserInfo() {
+    try {
+      const res = await api.get('/auth/getuserinfo')
+      setUserId(res.data.id)
+      setUserNickName(res.data.nickname)
+    }
+    catch(error) {
+      console.dir(error)
+    }
+  }
 
   const onLogoutClick = async () => {
-    try{
+    try {
       await api.post('/auth/signout', { sessionID: sessionID })
     }
     catch(error) {
       console.dir(error)
     }
     signOut()
+    getUserInfo()
   }
+
+  // useEffect(() => {
+  //   getUserInfo()
+  //   // eslint-disable-next-line
+  // }, [])
+
+  const { Column } = Grid
 
   return (
   // https://react.semantic-ui.com/augmentation/
   <Segment inverted>
     <Grid columns='equal'>
-      <Grid.Column>
-        <Button as={Link} to='/about'>About</Button>
-      </Grid.Column>
-      <Grid.Column>
-        <Button as={Link} to='/users'>Users</Button>
-      </Grid.Column>
-      <Grid.Column>
-        <Button as={Link} to='/login'>Sign in</Button>
-      </Grid.Column>
-      <Grid.Column>
-        <Button as={Link} to='/signup'>Sign up</Button>
-      </Grid.Column>
-      <Grid.Column>
+      <Column>
+        <Button
+          color='orange'
+          inverted
+          as={NavLink}
+          exact
+          to="/about"
+          style={isActive => ({backgroundColor:isActive ? "orange" : "purple"})}
+        >About</Button>
+      </Column>
+      <Column>
+        <Button
+          as={NavLink}
+          to='/users'
+          className={isActive => (!isActive ? 'active-button' : 'noactive-button')}
+        >Users</Button>
+      </Column>
+      <Column>
+        <Button
+          inverter
+          as={NavLink}
+          to='/login'
+          style={isActive => (
+            {
+              backgroundColor:isActive ? "orange" : "grey",
+              color:isActive ? "black" : "black",
+              borderRadius: 0,
+            }
+          )}
+        >Sign in</Button>
+      </Column>
+      <Column>
+        <Button color='twitter' as={Link} to='/signup'>Sign up</Button>
+      </Column>
+      <Column>
         <Button onClick={onLogoutClick}>Sign out</Button>
-      </Grid.Column>
-      <Grid.Column>
+      </Column>
+      <Column>
         <Button as={Link} to='/profile'>Profile</Button>
-      </Grid.Column>
-      <Grid.Column>
+      </Column>
+      <Column>
         <Button as={Link} to='/mygroups'>My Groups</Button>
-      </Grid.Column>
+      </Column>
+      <Column>
+        {userNickName}{'\n'}{userId}
+      </Column>
     </Grid>
   </Segment>
   );
