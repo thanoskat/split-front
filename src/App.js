5be04bcdb3906a78eed2be06bb338af57584a935
thanoskat <thanoskat@users.noreply.github.com>
2022-02-23 17:27:49 +0200

    test fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { Test, Notifications, UserBar, NavigationBar2, About, Users, User, Login, SignUp, VerifyLink, Profile, MyGroups, PrivateRoute, MainPage,MyGroupsCreateGroup } from './components'
+import { Group, Test, Notifications, UserBar, NavigationBar2, About, Users, User, Login, SignUp, VerifyLink, Profile, MyGroups, PrivateRoute, MainPage,MyGroupsCreateGroup } from './components'
 import { AuthenticationContextProvider } from './contexts/AuthenticationContext'
 import { GlobalStateContextProvider } from './contexts/GlobalStateContext'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
@@ -22,6 +22,7 @@ function App() {
               <Route path="/signup" component={SignUp} />
               {/* <PrivateRoute exact path="/group/:groupid"component={Group}/> */}
               <PrivateRoute path="/mygroups" component={MyGroups} />
+              <PrivateRoute path="/group/:groupid" component={Group} />
               <PrivateRoute exact path="/test" component={Test}/>
               <PrivateRoute exact path="/users" component={Users} />
               <PrivateRoute exact path="/profile" component={Profile} />
