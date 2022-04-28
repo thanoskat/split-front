b3edf6fdb858fad073674b86baafc21025347ed3
thanoskat <thanoskat@users.noreply.github.com>
2022-04-28 18:25:53 +0300

    label remove

--- src/components/AddExpense.jsx
+++ src/components/AddExpense.jsx
@@ -8,23 +8,19 @@ import store from '../redux/store'
 import IonIcon from '@reacticons/ionicons'
 
 function AddExpense({ close }) {
-
   const api = useAxios()
   const dispatch = useDispatch()
   const selectedGroup = store.getState().mainReducer.selectedGroup
   const sessionData = store.getState().authReducer.sessionData
   const abortControllerRef = useRef(null)
-
   const [loading, setLoading] = useState(false)
   const [includeAll, setIncludeAll] = useState(true)
   const [newExpense, setNewExpense] = useState({
     amount: '',
     description: '',
     labels: [],
-    participants: selectedGroup.members
+    participants: selectedGroup.members.map(member => member._id)
   })
-
-  console.log(newExpense)
 
   useEffect(() => {
     abortControllerRef.current = new AbortController();
@@ -63,6 +59,7 @@ function AddExpense({ close }) {
     }
     else {
       setNewExpense({...newExpense, labels: [...newExpense.labels, labelClickedId]})
+      // Uncomment if only one label is allowed
       // setNewExpense({...newExpense, labels: [labelClickedId]})
     }
   }
@@ -73,7 +70,6 @@ function AddExpense({ close }) {
     }
     else {
       setNewExpense({...newExpense, participants: [...newExpense.participants, participantClickedId]})
-      // setNewExpense({...newExpense, labels: [labelClickedId]})
     }
   }
 
@@ -83,38 +79,41 @@ function AddExpense({ close }) {
       setIncludeAll(false)
     }
     else {
-      setNewExpense({...newExpense, participants: [...selectedGroup.members]})
+      setNewExpense({...newExpense, participants: [...selectedGroup.members.map(member => member._id)]})
       setIncludeAll(true)
     }
   }
 
   return (
     <SlidingBox close={close} className='top-radius' style={{backgroundColor: 'var(--layer-1-color)'}}>
-      <div className='flex row t05 justcont-center alignitems-center padding4'>Create expense</div>
+      <div className='flex row t1 justcont-center alignitems-center padding4'>New Expense</div>
+      <div className='separator-0'/>
       <div className='flex column gap10 padding1010'>
         <input
-          className='input-field'
+          className='addexpense-input t3'
           placeholder='Amount'
           value={newExpense.amount}
           onChange={e => setNewExpense({...newExpense, amount: e.target.value })}
           spellCheck='false'
         />
         <input
-          className='input-field'
-          placeholder='Description'
+          className='addexpense-input t3'
+          placeholder='Description (optional)'
           value={newExpense.description}
           onChange={e => setNewExpense({...newExpense, description: e.target.value })}
           spellCheck='false'
         />
         <div className='flex row wrap gap10'>
           {selectedGroup.groupTags.map(label => (
-            <Label
-            key={label._id}
-            text={label.name}
-            color={label.color}
-            selected={newExpense.labels.includes(label._id)}
-            onClick={() => labelClicked(label._id)}/>
-          ))}
+            <div key={label._id} className={`pill pointer`}
+            style={
+              newExpense.labels.includes(label._id) ?
+              {color: 'var(--layer-0-color)', backgroundColor: label.color, borderColor: label.color} :
+              {color: label.color, backgroundColor: 'var(--layer-0-color)', borderColor: label.color}}
+              onClick={() => labelClicked(label._id)
+            }>
+              {label.name}
+            </div>))}
         </div>
         <div className='t4 medium flex row justcont-start alignitems-center gap6 pointer' onClick={includeAllClick}>
           <IonIcon className='t3' name={`${includeAll ? 'checkbox' : 'square-outline' }`}/>
@@ -123,18 +122,20 @@ function AddExpense({ close }) {
         {!includeAll &&
           <div className='flex row wrap gap10'>
             {selectedGroup.members.map(member => (
-              <Label
-              key={member._id}
-              text={member.nickname}
-              color={'#aaaaaa'}
-              selected={newExpense.participants.includes(member._id)}
-              onClick={() => participantClicked(member._id)}/>
-            ))}
+            <div key={member._id} className={`pill pointer`}
+            style={
+              newExpense.participants.includes(member._id) ?
+              {color: 'var(--layer-0-color)', backgroundColor: '#aaaaaa', borderColor: '#aaaaaa'} :
+              {color: '#aaaaaa', backgroundColor: 'var(--layer-0-color)', borderColor: '#aaaaaa'}}
+              onClick={() => participantClicked(member._id)
+            }>
+              {member.nickname}
+            </div>))}
           </div>
         }
-        <div className='flex row justcont-center' onClick={submitExpense}>
+        <div className='flex row justcont-center alignitems-center t2 gap8' onClick={submitExpense}>
           Submit
-          {loading && <IonIcon name='sync' className='t3 spin'/>}
+          {loading && <IonIcon name='sync' className='t2 spin'/>}
         </div>
       </div>
     </SlidingBox>
