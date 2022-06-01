7e96071a02e9aaad810e913b1cadf84fdca03415
ChristosKehayas <christos.kechagia@gmail.com>
2022-06-01 02:40:34 +0100

    login page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -10,39 +10,61 @@ const Login = () => {
   const [linkText, setLinkText] = useState('')
 
   const formSubmit = async (e) => {
+
+
     e.preventDefault();
     try {
       const res = await axios.post(`${process.env.REACT_APP_APIURL}/auth/sendlink`, { email: email })
-      if(res && res.data && res.data.link) {
-        if(res.data === "Cannot read property '_id' of null") {
-          setMessage("Email is not correct!")
+      if (res && res.data && res.data.link) {
+        if (res.data === "Cannot read property '_id' of null") {
+          setMessage("The email you entered does not correspond to an account. Please try again.")
           setLinkText('')
         }
         else {
           setLink(res.data.link)
-          setMessage("Email sent!")
           setLinkText('EMAIL LINK!')
         }
       }
     }
-    catch(error) {
+    catch (error) {
       setMessage(error.message)
     }
   }
 
   const keyPress = (e) => {
-    if(e.key === 'Enter') {
+    if (e.key === 'Enter') {
       formSubmit(e)
     }
   }
-
+//check text overflows
   return (
-    <div>
-      <div className='login-box'>
-        <input placeholder="Email" value={email} onInput={e => setEmail(e.target.value)} onKeyPress={keyPress}/>
-        <button onClick={formSubmit}>Submit</button>
+    <div id="loginpage" className=' flex column ' style={{ color: "var(--light-color)" }}>
+      <div className='logo t66 flex alignitems-center'>
+        α
+      </div>
+      <div className='appName flex column alignitems-center t05'>
+        αlphaSplit
+        <div className='appDescr t5'>
+          The tool for organising your shared finances.
+        </div>
+      </div>
+      <div className='loginBox flex column ' style={{ backgroundColor: "var(--layer-1-color)", borderColor: "var(--layer-1-color)", borderStyle: "solid" }}>
+        <div className='signUpMsg' style={{ color: "var(--light-color)" }}>
+          Enter your email to log in.
+        </div>
+        <div className='mailbox flex column '>
+          <input className={`styledInput ${message === "" ? null : "inputErr"}`} placeholder='you@email' value={email} onInput={e => setEmail(e.target.value)} onKeyPress={keyPress} />
+          <div className='mailmsg t6'>{message}</div>
+        </div>
+        <button className={`shadow login-button flex justcont-center relative ${email !== "" ? "active" : null}`} disabled={email === "" ? true : false} onClick={formSubmit}>
+          Log In
+        </button>
         <a href={link}>{linkText}</a>
-        <div>{message}</div>
+        <div className="or flex justcont-center alignitems-center" style={{ color: "var(--light-color)" }}>OR</div>
+        <div className='shadow signUp-button flex justcont-center relative'>
+          Create new account
+        </div>
+
       </div>
     </div>
   );
