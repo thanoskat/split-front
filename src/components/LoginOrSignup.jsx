894d296096af808b23dccf3bbefe473ac5489a6e
thanoskat <thanoskat@users.noreply.github.com>
2022-06-25 17:42:21 +0300

    verify link from another device

--- src/components/LoginOrSignup.jsx
+++ src/components/LoginOrSignup.jsx
@@ -2,35 +2,72 @@ import '../style/Login.css'
 import { useState } from 'react'
 import axios from 'axios'
 import { Link } from 'react-router-dom'
-
-
+import { useNavigate } from 'react-router-dom';
+import { signIn } from '../redux/authSlice'
+import { useDispatch } from 'react-redux'
 
 const LoginOrSignup = () => {
 
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
   const [email, setEmail] = useState('')
   const [message, setMessage] = useState('')
-  const [link, setLink] = useState('')
+  const [link, ] = useState('')
   const [linkText, setLinkText] = useState('')
+  const [loginEmailSent, setLoginEmailSent] = useState(false)
+  const [sessionUnique, setSessionUnique] = useState('')
 
   const loginSubmit = async (e) => {
-    e.preventDefault();
+    e.preventDefault()
     try {
-      const res = await axios.post(`${process.env.REACT_APP_APIURL}/auth/sendlink`, { email: email })
-      if (res && res.data && res.data.link) {
-        if (res.data === "Cannot read property '_id' of null") {
-          setMessage("The email you entered does not correspond to an account. Please try again.")
+      const res = await axios.post(`${process.env.REACT_APP_APIURL}/auth/sendsigninlink`, { email })
+      if(res && res.data) {
+        if(res.data === `Cannot read property '_id' of null`) {
+          setMessage('The email you entered does not correspond to an account. Please try again.')
           setLinkText('')
         }
         else {
-          setLink(res.data.link)
-          setLinkText('EMAIL LINK!')
+          setLoginEmailSent(true)
+          setSessionUnique(res.data.unique)
+          console.log(res.data.unique)
         }
       }
     }
-    catch (error) {
-      setMessage(error.message)
+    catch(error) {
+      console.log(error)
     }
   }
+
+  const continueSignIn = async (unique) => {
+    try {
+      const res = await axios.post(`${process.env.REACT_APP_APIURL}/auth/signin`, { unique: unique }, { withCredentials: true })
+      dispatch(signIn({accessToken: res.data.accessToken, sessionData: res.data.sessionData}))
+      navigate('/')
+    }
+    catch(error) {
+      console.log(error.message)
+    }
+  }
+
+  // const loginSubmit = async (e) => {
+  //   e.preventDefault()
+  //   try {
+  //     const res = await axios.post(`${process.env.REACT_APP_APIURL}/auth/sendlink`, { email: email })
+  //     if (res && res.data && res.data.link) {
+  //       if (res.data === `Cannot read property '_id' of null`) {
+  //         setMessage('The email you entered does not correspond to an account. Please try again.')
+  //         setLinkText('')
+  //       }
+  //       else {
+  //         setLink(res.data.link)
+  //         setLinkText('EMAIL LINK!')
+  //       }
+  //     }
+  //   }
+  //   catch (error) {
+  //     setMessage(error.message)
+  //   }
+  // }
 
   const loginkeyPress = (e) => {
     if (e.key === 'Enter') {
@@ -38,29 +75,34 @@ const LoginOrSignup = () => {
     }
   }
 
-  return (
-    <div className='loginBox flex column ' style={{ backgroundColor: "var(--layer-1-color)", borderColor: "var(--layer-1-color)", borderStyle: "solid" }}>
-      <div className='signUpMsg' style={{ color: "var(--light-color)" }}>
+  return(
+    <div className='loginBox flex column' style={{ backgroundColor: 'var(--layer-1-color)', borderColor: 'var(--layer-1-color)', borderStyle: 'solid' }}>
+      <div className='signUpMsg' style={{ color: 'var(--light-color)' }}>
         Enter your email to log in.
       </div>
-      <form className="flex column" onSubmit={loginSubmit}>
-        <div className='mailbox flex column '>
-          <input className={`styledInput ${message === "" ? null : "inputErr"}`} placeholder='you@email' value={email} onChange={e => setEmail(e.target.value)} onKeyPress={loginkeyPress} />
+      <form className='flex column' onSubmit={(e) => loginSubmit(e)}>
+        <div className='mailbox flex column'>
+          <input className={`styledInput ${message === '' ? null : 'inputErr'}`} placeholder='you@email' value={email} onChange={e => setEmail(e.target.value)} onKeyPress={loginkeyPress} />
           <div className='mailmsg t6'>{message}</div>
         </div>
-        <button type="submit" className={`shadow login-button flex justcont-center relative ${email !== "" ? "active" : null}`} disabled={email === "" ? true : false} >
+        {!loginEmailSent &&
+        <button type='submit' className={`shadow login-button flex justcont-center relative ${email !== '' ? 'active' : null}`} disabled={email === '' ? true : false}>
           Log In
-        </button>
+        </button>}
         <a href={link}>{linkText}</a>
-        <div className="or flex justcont-center alignitems-center" style={{ color: "var(--light-color)" }}>OR</div>
-        <Link to="/signup" style={{ textDecoration: 'none' }}>
+        <div className='or flex justcont-center alignitems-center' style={{ color: 'var(--light-color)' }}>OR</div>
+        <Link to='/signup' style={{ textDecoration: 'none' }}>
           <div className='shadow signUp-button flex justcont-center relative' >
             Create new account
           </div>
         </Link>
       </form>
-    </div>)
+      {loginEmailSent &&
+      <button className={`shadow login-button flex justcont-center relative active`} onClick={() => continueSignIn(sessionUnique)}>
+        Continue
+      </button>}
+    </div>
+  )
 }
 
-
-export default LoginOrSignup;
+export default LoginOrSignup
